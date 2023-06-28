11c5dcc6a6d8801c7184418d94446fdf43b1c541
Rocío Zavala <66143482+rzavala1@users.noreply.github.com>
2023-06-28 10:34:56 -0600

    se agrega lista de hits para pagina
    se agrega lista de hits para pagina

---
+++ src/components/@atoms/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoadingSpinner: React.FC = () => {
+  return <p>Loading...</p>;
+};
+
+export default LoadingSpinner;
---
+++ src/components/@organisms/HitsList.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Paper, Table, TableContainer, Typography } from '@mui/material';
+import { useRouter } from 'next/router';
+import { useHitsService } from '@/@service/HitsService';
+import TableOptions from '../@molecules/TableOptions';
+
+interface Hit {
+    target: string;
+    status: string;
+    assigned: string;
+}
+
+const HitsList: React.FC = () => {
+    const [hits, setHits] = useState([]);
+    const { data } = useHitsService();
+    const router = useRouter();
+
+    useEffect(() => {
+        setHits(data?.hits);
+    }, [data]);
+
+    return (
+        <>
+            <Box sx={{ maxWidth: '100%', mx: 'auto' }}>
+                <Typography variant="h4" align="center">
+                    Hits
+                </Typography>
+                <Box sx={{ maxWidth: '1280px', marginTop: 3 }}>
+                    <TableOptions
+                        rows={["Tarjet", "Estatus", "Asignado", ""]}
+                        data={
+                            data?.hits?.map((v: any, index: number) => ({
+                                title: v?.id,
+                                columns: [
+                                    <p key={index}>{v?.target}</p>,
+                                    <p key={index}>{v?.status}</p>,
+                                    <p key={index}>{v?.hitman?.user?.name}</p>,
+                                    <div className="flex" key={index}>
+                                        <button
+                                            onClick={() => {
+                                                router.push(`/locations/edit/${v?.id}`);
+                                            }}
+                                        >
+                                            Editar
+                                        </button>
+                                    </div>,
+                                ],
+                            })) || []
+                        }
+                    />
+                </Box>
+
+
+            </Box>
+        </>
+    );
+};
+
+export default HitsList;
